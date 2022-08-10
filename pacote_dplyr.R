2fa2bc7000b594b4a9e92d1548554c64faf58b89
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-09 23:24:30 -0300

    Add files via upload

---
+++ pacote_dplyr.R
@@ -0,0 +1,16 @@
+
+# Pacote dplyr -----------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 09/08/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://github.com/rstudio/cheatsheets/blob/main/data-transformation.pdf -----------------------------------------------------
+
+# Introdução -------------------------------------------------------------------------------------------------------------------------------
+
+### As funções do dplyr trabalha com pipes dados organizados (tidy data).
+
+# Summarise Cases --------------------------------------------------------------------------------------------------------------------------
+
+### Aplica funções de resumos a colunas para criar novas tabelas de
+### estatísticas resumos. Funções de resumo toma vetores como input
+### e retorna um valor, por exemplo, cálculos de médias, desvios-
+### padrão e erros-padrão.
