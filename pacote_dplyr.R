7ed99135e9fdb9e00fe026659c40119bac48046e
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-08-09 23:28:22 -0300

    Add files via upload

--- pacote_dplyr.R
+++ pacote_dplyr.R
@@ -8,9 +8,24 @@
 
 ### As funções do dplyr trabalha com pipes dados organizados (tidy data).
 
+library(dplyr)
+
 # Summarise Cases --------------------------------------------------------------------------------------------------------------------------
 
 ### Aplica funções de resumos a colunas para criar novas tabelas de
 ### estatísticas resumos. Funções de resumo toma vetores como input
 ### e retorna um valor, por exemplo, cálculos de médias, desvios-
 ### padrão e erros-padrão.
+
+## Função summarise:
+
+### Computa tabelas de resumos.
+
+summarise(mtcars, media = mean(mpg))
+
+## Função count:
+
+### Conta número de linhas de cada grupo definido pelas variáveis...
+### Também tally().
+
+count(mtcars, cyl)
